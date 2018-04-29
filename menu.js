3a622790f34a1e00748efebd9642cc1a39edcc69
honginho <sm870831@gmail.com>
2018-04-29 10:25:33 +0800

    add brief translated menu

---
+++ menu.js
@@ -0,0 +1,23 @@
+var translate = {
+  'c':{
+      '1', // 招牌便當',
+      '2', // 番茄牛腩',
+      '3', // 招牌雞腿',
+      '4', // 招牌香雞排',
+      '5' // 滷雞腿'
+    }
+  'v':{
+      'Cơm hộp gia truyền',
+      'Sườn bò sốt cà',
+      'Đùi gà gia truyền ',
+      'Gà rán chiên giòn gia truyền',
+      'Đùi gà kho'
+    }
+  'i':{
+      'daging + nasi kotak',
+      'Sirloin tomat',
+      'kaki ayam',
+      'steak daging ayam',
+      'Kaki ayam direbus'
+    }
+}
